7997d5a6adf90d45c716cda4e17dee7b65170ad5
Arpit Jain <arpit.jan10@gmail.com>
2022-02-16 13:34:38 -0700

    broken our homepage into smaller componenets, finished styling the menu item component.

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './directory.styles.scss';
+import MenuItem from '../menu-item/menu-item.component';
+/** Has to be  a class component because this has to store the state value of the menu items that we want to pass and create menu items with. */
+
+class Directory extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            section: [{
+                title: 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1,
+                linkUrl: 'shop/hats'
+              },
+              {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: 'shop/jackets'
+              },
+              {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: 'shop/sneakers'
+              },
+              {
+                title: 'womens',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                size: 'large',
+                id: 4,
+                linkUrl: 'shop/womens'
+              },
+              {
+                title: 'mens',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                size: 'large',
+                id: 5,
+                linkUrl: 'shop/mens'
+              }
+            ]
+        }
+    }
+
+    render(){
+        return(
+            <div className='directory-menu'>
+                {
+                    /** this.state.section.map(section=>(   we can do this or destructure it so that we do not have to use 'section.' to call every element of section
+                        <MenuItem title={section.title}/>
+                    ))*/
+
+                    this.state.section.map(({title, imageUrl, id, size}) => 
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}  />
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import './homepage.styles.scss'
+// import MenuItem from '../../components/menu-item/menu-item.component';
+import Directory from '../../components/directory/directory.component';
+
+
 const homepage = () => (
     <div className = 'homepage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
+        <Directory />
+        {/* <div className='directory-menu'>
+            <MenuItem title={"HATS"} subtitle={"SHOP NOW"} />
+            <MenuItem title={"JACKETS"} subtitle={"SHOP NOW"} />
+            <MenuItem title={"SNEAKERS"} subtitle={"SHOP NOW"} />
+            <MenuItem title={"WOMENS"} subtitle={"SHOP NOW"} />
+            <MenuItem title={"MENS"} subtitle={"SHOP NOW"} /> */}
+            {/* <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'> HATS </h1>
                     <span className='subtitle'> SHOP NOW</span>
                 </div>
-            </div>
-            <div className='menu-item'>
+            </div> */}
+            {/* <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'> JACKETS </h1>
                     <span className='subtitle'> SHOP NOW</span>
                 </div>
-            </div>
-            <div className='menu-item'>
+            </div> */}
+            {/* <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'> SNEAKERS </h1>
                     <span className='subtitle'> SHOP NOW</span>
                 </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
+            </div> */}
+            {/* <div className='menu-item'> */}
+                {/* <div className='content'>
                     <h1 className='title'> WOMENS </h1>
                     <span className='subtitle'> SHOP NOW</span>
                 </div>
@@ -32,8 +42,8 @@ const homepage = () => (
                     <h1 className='title'> MENS </h1>
                     <span className='subtitle'> SHOP NOW</span>
                 </div>
-            </div>
-        </div>
+            </div> */}
+        {/* </div> */}
     </div>
 )
 
