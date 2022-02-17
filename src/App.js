286ee47859a92fd730a0e7a7151d2e8a4e551854
Arpit Jain <arpit.jan10@gmail.com>
2022-02-17 14:37:40 -0700

    updating the app with a shop page, collection rpreview component and collection item componenet.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () =>(
   <div>
@@ -15,6 +15,7 @@ function App() {
       <Switch>
         {/* <HomePage /> */}
         <Route exact path='/' component = {HomePage} />
+        <Route exact path='/shop' component = {ShopPage} />
         <Route exact path='/hats' component = {HatsPage} />
       </Switch>   
     </div>
